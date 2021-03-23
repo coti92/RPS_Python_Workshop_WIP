63dbab5aab1a4df998f7e098ca1516bfa8bfbeec
David José Cotilla Martín <cotidavid@gmail.com>
2021-03-23 23:50:08 +0100

    new way to select winner

--- labelconstans.py
+++ labelconstans.py
@@ -1,3 +1,5 @@
 class LabelConstants(object):
     
      LABELS = {"R":"rock","P":"paper","S":"scissor"}
+     RPS_TABLE = [[-1, 1, 0], [1, -1, 2], [0, 2, -1]]
+     RPS_VALUE = {"rock" : 0,"paper" : 1,"scissor" : 2}
